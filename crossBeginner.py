1b8e3fc95d00d21aff3ffda867a20b95edde38b9
ReinRaus <reinr_000@Lenovo-PC>
2016-06-06 12:01:40 +0300

    Добавлен первый кроссворд Begginer с сайта regexcrosswords

---
+++ crossBeginner.py
@@ -0,0 +1,23 @@
+# https://regexcrossword.com/challenges/beginner/puzzles/
+import rectMap as rect
+
+class simpleCross():
+    def __init__( values ):
+        self.maps = values[ "maps" ]
+        self.regexs = values[ "regexs" ]
+        self.printer = values[ "printer" ]
+        self.allLen = values[ "allLen" ]
+
+cross = []
+
+cross.append( simpleCross( {
+    "maps":    rect.getMap( 2, 2 ),
+    "allLen":  rect.getLens( 2, 2 ),
+    "printer": rect.getPrinter( 2, 2 ),
+    "regexs": [
+        r"HE|LL|O+",
+        r"[PLEASE]+",
+        r"[^SPEAK]+",
+        r"EP|IP|EF"
+        ]
+    } ) )
