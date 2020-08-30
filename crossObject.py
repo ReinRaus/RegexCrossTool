a90b0f58fb1d3500edf457252fb550d1d9576442
ReinRaus <reinraus.nax@gmail.com>
2020-08-30 07:17:46 +0300

    Неожиданно, 4 года назад не добавил один файл в репозиторий
    А значит никто не смог бы запустить весь приведенный код, а раз жалоб не было, значит никто и не пытался :)

---
+++ crossObject.py
@@ -0,0 +1,6 @@
+class crossObject:
+    def __init__( self, maps, allLen, printer, regexs ):
+        self.maps = maps
+        self.allLen = allLen
+        self.printer = printer
+        self.regexs = regexs
--- launcher.py
+++ launcher.py
@@ -1,3 +1,3 @@
 import regexCrossTool as tool, crossHexa13 as cross, crossBeginner as cross2
-res = tool.Tool( cross )
+res = tool.Tool( cross.cross[0] )
 res.print()
